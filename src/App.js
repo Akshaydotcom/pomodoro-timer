13674c703c8dc975303a556afb407bffb7715558
AkshayGB <akshaybhandaryg1@gmail.com>
2022-06-23 06:55:04 -0700

    Added clock component and settings for app

--- src/App.js
+++ src/App.js
@@ -2,32 +2,53 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Tabs, Tab } from 'react-bootstrap';
-
-
+import { ToggleButton } from 'react-bootstrap'
+import { ClockComponent } from './components/ClockComponent';
 function App() {
-  const [darkMode, setDarkMode]=useState(true)
+  const [darkMode, setDarkMode]=useState(false)
+  const [format24, setFormat24]=useState(false)
   const [key, setKey]=useState("Pomodoro")
   const changeMode=()=>{
     if(darkMode) setDarkMode(false)
     else setDarkMode(true)
   }
+  const changehourFormat=()=>{
+    if(format24) setFormat24(false)
+    else setFormat24(true)
+  }
   return (
-    <div className={darkMode?"App row":"body-dark App row"}>
-      {/* <button onClick={changeMode}/> */}
-      <div className='col-lg-3'></div>
-      <div className='middle col-lg-3'>
+    <div className={darkMode?"App body-light row":"body-dark App row"}>
+      <div className='col-lg-4'></div>
+      <div className='col-lg-4'>
+      
+        <div className='middle'>
         <Tabs activeKey={key} onSelect={(k)=>setKey(k)}>
           <Tab eventKey="Pomodoro" title="Pomodoro" >
             Lorem Ipsum Clock comes here
+            <ClockComponent minutes={1} seconds={59} src={"Pomodoro"}/>
           </Tab>
           <Tab eventKey="ShortBreak" title="Short Break" >
           Lorem Ipsum Clock comes here in short break
+          <ClockComponent minutes={1} seconds={59} src={"Short Break"}/>
           </Tab>
           <Tab eventKey="LongBreak" title="Long Break" >
           Lorem Ipsum Clock comes here in long break
+          <ClockComponent minutes={1} seconds={59} src={"Long Break"}/>
+          </Tab>
+          <Tab eventKey="Timer Settings" title="Timer Settings">
+            <div>
+              Pomodoro Time format
+            </div>
+            <label>Focus Time</label><input type="number"></input><br/>
+            <label>Short Break</label><input type="number"></input><br/>
+            <label>Long Break</label><input type="number"></input><br/>
+            <label>Dark Mode when running: </label><button onClick={changeMode}>change mode</button><br/>
+            <label>Hour format: </label><ToggleButton onClick={changehourFormat}>change format</ToggleButton><br/>
           </Tab>
         </Tabs>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 }
--- src/components/ClockComponent.js
+++ src/components/ClockComponent.js
@@ -1,1 +1,45 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+
+export const ClockComponent=(props)=> {
+  const[minutes,SetMinutes]=useState(props.minutes);
+  const[seconds,setSeconds]=useState(props.seconds);
+  useEffect(()=>{
+    SetMinutes(props.minutes);
+    setSeconds(props.seconds);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  
+  useEffect(()=>{
+    const minutesInterval = setInterval(()=>{
+      if(minutes>0)
+        SetMinutes(minutes-1)
+    },60000)
+    return ()=>{
+      clearInterval(minutesInterval)
+    };
+  },[minutes])
+
+useEffect(() => {
+  const secondsinterval = setInterval(() => {
+    if(seconds>0)
+      setSeconds(seconds-1)
+    else if(seconds===0)
+      setSeconds(59)}, 1000);
+  
+  return () => {
+    clearInterval(secondsinterval);
+  };
+}, [seconds]);
+  
+  return (<div>
+    <div>
+          <div id="Minutes">{minutes}</div>
+          <span id="mins">Minutes</span>
+        </div>
+        <div>
+          <div id="Seconds">{seconds}</div>
+          <span id="seconds">Seconds</span>
+        </div>
+      </div>);
+}
